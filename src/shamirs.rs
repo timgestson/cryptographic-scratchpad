3c9eb5b915b34e20ceb6be53dc7f88ed7703afe3
Tim Gestson <tgestson@gmail.com>
2022-04-21 19:14:49 -0400

    Rearange repo

---
+++ src/shamirs.rs
@@ -0,0 +1,65 @@
+use rand::prelude::*;
+use std::vec::Vec;
+use math::{fields::f64::BaseElement as Felt};
+
+/* Shamir's Secret Sharing Algorithm adapted from https://en.wikipedia.org/wiki/Shamir%27s_Secret_Sharing
+// Using Winterfell's FieldElements so I dont have to reimplement Finite Fields
+```
+let secret = Felt::new(43253243242);
+// Split the secret into 6 points of a quadratic polynomial
+let vector = split_secret(secret);
+// Perform lagrange interpolation on 3 points to determine the y intercept (x = 0)
+let result = lagrange_interpolation(Felt::new(0), vector[0..3].to_vec());
+println!("{}", result);
+```
+*/
+
+fn split_secret(secret: Felt) -> Vec<(Felt, Felt)> {
+    let (a, b) = generate_two_randoms();
+    let mut pairs = Vec::<(Felt, Felt)>::new();
+    for x in 1..6 {
+        pairs.push(calculate_polynomial_pairs(secret, a, b, Felt::new(x)));
+    }
+    pairs
+}
+
+fn calculate_polynomial_pairs(a0: Felt, a1: Felt, a2: Felt, x: Felt) -> (Felt, Felt){
+    (x, a0 + a1 * x + a2 * x * x)
+}
+
+fn generate_two_randoms() -> (Felt, Felt) {
+    let a = random::<u64>();
+    let b = random::<u64>();
+    (Felt::new(a), Felt::new(b))
+}
+
+fn lagrange_interpolation(x: Felt, points: Vec<(Felt, Felt)>) -> Felt {
+    let mut result = Felt::new(0);
+    let points_len = points.len();
+    for i in 0..points_len {
+        let mut acc = points[i].1;
+        for j in 0..points_len {
+            if i != j {
+                let den = points[i].0 - points[j].0;
+                let num = x - points[j].0;
+                acc *= num / den;
+            }
+        }
+        result += acc;
+    }
+    result
+}
+
+#[test]
+fn test_shamirs_secret(){
+    let secret = Felt::new(10);
+    let vector = split_secret(secret);
+    // Assert 3 points gives the correct y intercept
+    assert_eq!(secret, lagrange_interpolation(Felt::new(0), vector[0..3].to_vec()));
+    // Assert a different 3 points gives the correct y intercept
+    assert_eq!(secret, lagrange_interpolation(Felt::new(0), vector[2..5].to_vec()));
+    // Assert 4 points gives the correct y intecept 
+    assert_eq!(secret, lagrange_interpolation(Felt::new(0), vector[0..4].to_vec()));
+    // Assert 2 points does not give up the secret
+    assert_ne!(secret, lagrange_interpolation(Felt::new(0), vector[0..2].to_vec()));
+}
