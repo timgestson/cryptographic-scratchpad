5c2feb6931f62ba094a734b6dda84b299e1f71f4
Tim Gestson <tgestson@gmail.com>
2022-04-22 20:56:13 -0400

    Better comments

--- src/berlekampmassey.rs
+++ src/berlekampmassey.rs
@@ -3,11 +3,10 @@ use math::{fields::f64::BaseElement as Felt, FieldElement};
 use rand::prelude::*;
 use std::vec::Vec;
 
-// Berlekamp-Massey Algorithm
-// https://en.wikipedia.org/wiki/Berlekamp–Massey_algorithm
-// Find the minimal polynomial of a linearly recurrent sequence on a finite field.
+// Using Berlekamp-Massey Algorithm
+// to find the shortest linear recurrence
+// as stated here https://mzhang2021.github.io/cp-blog/berlekamp-massey/
 fn berlekamp_massey(series: &[Felt]) -> Vec<Felt> {
-    // Using c becasue it is referred to as this in the papers
     let mut c = Vec::<Felt>::new();
     let mut old_c = Vec::<Felt>::new();
     let mut best_c_index_failure: Option<usize> = None;
@@ -37,7 +36,8 @@ fn berlekamp_massey(series: &[Felt]) -> Vec<Felt> {
                 let mut d: Vec<Felt> = old_c.iter().map(|&elem| Felt::ZERO - elem).collect();
                 // insert 1 on left
                 d.insert(0, Felt::ONE);
-                // multiply the sequence by delta / d(f + 1)
+                
+                // series * delta / d(f + 1)
                 let df1 = d
                     .iter()
                     .enumerate()
@@ -45,7 +45,6 @@ fn berlekamp_massey(series: &[Felt]) -> Vec<Felt> {
                     .fold(Felt::ZERO, |a, b| a + b);
 
                 let coef = delta / df1;
-
                 d = d.iter().map(|&elem| elem * coef).collect();
                 for _ in 0..(i - index - 1) {
                     d.insert(0, Felt::ZERO);
@@ -99,6 +98,17 @@ fn test_berlekamp_massey() {
     // 1,3,5,11,25,59,141,339
     // 3 (s-1) - 1 (s-2) - 1 (s-3)
     let series: Vec<Felt> = [1_u64, 3, 5, 11, 25, 59, 141, 339]
+        .iter()
+        .map(|&num| Felt::new(num))
+        .collect();
+    assert_eq!(
+        vec![Felt::new(3), Felt::ZERO - Felt::ONE, Felt::ZERO - Felt::ONE],
+        berlekamp_massey(&series)
+    );
+    
+    // 2,2,1,2,1,191,393,132
+    // 3 (s-1) - 1 (s-2) - 1 (s-3)
+    let series: Vec<Felt> = [1_u64,2,1,2,1,2]
         .iter()
         .map(|&num| Felt::new(num))
         .collect();
